0d31ed2c4d5f5210f7addb49442b3a09be876c7c
harotaylo <harotaylo1@gmail.com>
2026-06-17 09:14:35 -0400

    Import Supabase

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import axios from 'axios';
 import cors from 'cors';
+import { createClient } from '@supabase/supabase-js';
 
 const app = express();
 app.use(express.json());
