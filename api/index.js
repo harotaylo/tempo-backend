250b6a0d6b7be64e25577481f708cd4bc7899daa
harotaylo <harotaylo1@gmail.com>
2026-06-17 09:12:08 -0400

    Add cors

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import axios from 'axios';
+import cors from 'cors';
 const app = express();
+app.use(cors());
 app.get('/', (req, res) => res.json({ test: 'ok' }));
 export default app;
